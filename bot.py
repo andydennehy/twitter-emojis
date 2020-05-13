41c7ac83176dfdc649bff743ca53aba6083a8163
andydennehy <44749277+andydennehy@users.noreply.github.com>
2020-05-13 10:02:04 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,96 @@
+# Basic libraries
+import time
+import tweepy
+import json
+import numpy as np
+import preprocessor as p
+
+# Emoji libraries. Requires pyTorch == 1.0.1
+import emoji
+
+from bot.torchMoji.torchmoji.sentence_tokenizer import SentenceTokenizer
+from bot.torchMoji.torchmoji.model_def import torchmoji_emojis
+from bot.torchMoji.torchmoji.global_variables import PRETRAINED_PATH, VOCAB_PATH
+
+# API keys
+consumer_key = "XXX"
+consumer_secret = "XXX"
+access_token = "XXX"
+access_token_secret = "XXX"
+
+# Filter by username
+user_name = "XXX"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+auth.secure = True
+
+
+EMOJIS = ":joy: :unamused: :weary: :sob: :heart_eyes: \
+:pensive: :ok_hand: :blush: :heart: :smirk: \
+:grin: :notes: :flushed: :100: :sleeping: \
+:relieved: :relaxed: :raised_hands: :two_hearts: :expressionless: \
+:sweat_smile: :pray: :confused: :kissing_heart: :heartbeat: \
+:neutral_face: :information_desk_person: :disappointed: :see_no_evil: :tired_face: \
+:v: :sunglasses: :rage: :thumbsup: :cry: \
+:sleepy: :yum: :triumph: :hand: :mask: \
+:clap: :eyes: :gun: :persevere: :smiling_imp: \
+:sweat: :broken_heart: :yellow_heart: :musical_note: :speak_no_evil: \
+:wink: :skull: :confounded: :smile: :stuck_out_tongue_winking_eye: \
+:angry: :no_good: :muscle: :facepunch: :purple_heart: \
+:sparkling_heart: :blue_heart: :grimacing: :sparkles:".split(' ')
+
+
+def top_elements(array, k):
+    ind = np.argpartition(array, -k)[-k:]
+    return ind[np.argsort(array[ind])][::-1]
+
+
+def find_emoji(tweet):
+    tweet = p.clean(tweet)
+
+    # Tokenizing using dictionary
+    with open(VOCAB_PATH, 'r') as f:
+        vocabulary = json.load(f)
+
+    st = SentenceTokenizer(vocabulary, 30)
+
+    # Loading model
+    model = torchmoji_emojis(PRETRAINED_PATH)
+    # Running predictions
+    tokenized, _, _ = st.tokenize_sentences([tweet])
+    # Get sentence probability
+    prob = model(tokenized)[0]
+
+    # Top emoji id
+    emoji_id = top_elements(prob, 1)
+
+    # map to emojis
+    tweet_emoji = map(lambda x: EMOJIS[x], emoji_id)
+    return emoji.emojize(' '.join(tweet_emoji), use_aliases=True)
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        super().__init__()
+        self.api = api
+
+    def on_status(self, status):
+        if len(str(status).split(" ")) > 0 and not status.in_reply_to_status_id and not status.retweeted:
+            tweet_emoji = find_emoji(status.text)
+            self.api.update_status(tweet_emoji + " https://twitter.com/realdonaldtrump/status/" + str(status.id))
+            print("Tweeted! Sleeping for 30 mins to not spam...")
+            time.sleep(60*30)
+            print("Woke up :D")
+
+
+def main():
+    api = tweepy.API(auth)
+
+    listener = MyStreamListener(api)
+    stream = tweepy.Stream(auth=api.auth, listener=listener)
+    stream.filter(follow=[str(api.get_user(screen_name=user_name).id)])
+
+
+if __name__ == '__main__':
+    main()
